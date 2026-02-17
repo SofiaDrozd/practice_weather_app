238e8598262ae353961cc82d5b038d29c2ab9314
SofiaDrozd <drozdsofia2005@gmail.com>
2026-02-17 14:52:26 -0900

    move API to .env file and add .env to .gitignore

--- src/assets/api/index.js
+++ src/assets/api/index.js
@@ -1,6 +1,6 @@
 function getWeather (options) {
   const { name } = options
-  const API_KEY = 'a5cfa61fa633440988a122551261602'
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY
 
   return fetch(
     `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${name}&lang=uk`
